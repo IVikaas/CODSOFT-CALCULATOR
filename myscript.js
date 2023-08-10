5938ba7114661809cdaacb1261a8d26bbcd481bf
Vikas Kumar <96311829+IVikaas@users.noreply.github.com>
2023-08-10 23:59:10 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,60 @@
+// let input = document.getElementById('inputBox');
+// let buttons = document.querySelectorAll('button');
+
+// let string = "";
+// let arr = Array.from(buttons);
+
+// arr.forEach(button => {
+//   button.addEventListener('click', (e) =>{
+//     if(e.target.innerHTML == '='){
+//       string = eval(string);
+//       input.value = string;
+//     }
+    
+//     else if(e.target.innerHTML == 'AC'){
+//       string = "";
+//       input.value = string;
+//     }
+//     else if(e.target.innerHTML == '⌫'){
+//       string = string.substring(0, string.length-1);
+//       input.value = string;
+//     }
+//     else{
+//       string += e.target.innerHTML;
+//       input.value = string;
+//     }
+//   })
+// })
+
+
+
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+
+arr.forEach(button => {
+  button.addEventListener('click', (e) => {
+    if (e.target.innerHTML === '=') {
+      string = eval(string);
+      input.value = string;
+    } else if (e.target.innerHTML === 'AC') {
+      string = "";
+      input.value = string;
+    } else if (e.target.innerHTML === '⌫') {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else if (e.target.innerHTML === 'x') {
+      string += '*'; // Change 'x' to '*'
+      input.value = string;
+    } else {
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
+
+
+
+
